8e6f10b35e02cae8ec19e95727cdcbb09172791f
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2023-05-23 11:39:46 -0300

    Comentários

--- src/fundamentos/PrimeiroPrograma.java
+++ src/fundamentos/PrimeiroPrograma.java
@@ -3,6 +3,11 @@ package fundamentos;
 public class PrimeiroPrograma {
     public static void main(String[] args) {
 
+        // Uma sentença de código termina com ;
+        /*
+        * Linha 1
+        * Linha 2
+        */
         System.out.println("Primeiro Programa #01");
         System.out.println("Primeiro Programa #02");
     }
